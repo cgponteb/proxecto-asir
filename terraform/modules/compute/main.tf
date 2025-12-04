cdb908011c8ad487cfdd250c2f4f7caecb232a5d
Carlos Ponte <108192344+cgponteb@users.noreply.github.com>
2025-12-04 14:59:01 +0100

    Increasing minimum LB instances to 2 for HA

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -85,9 +85,9 @@ resource "aws_autoscaling_group" "app" {
   health_check_type         = "ELB"
   health_check_grace_period = 300
 
-  min_size         = 1
+  min_size         = 2
   max_size         = 3
-  desired_capacity = 1
+  desired_capacity = 2
 
   launch_template {
     id      = aws_launch_template.app.id
